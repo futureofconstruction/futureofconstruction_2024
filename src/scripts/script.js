38e525d189e7dc541386e7cd0f1562d0deb571c8
Untangled Entangled <praises_sauces.0i@icloud.com>
2024-05-31 13:54:00 +0200

    Small adjustments

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -15,8 +15,26 @@ const observer = new IntersectionObserver((entries) => {
   document.addEventListener('DOMContentLoaded', function () {
     const button = document.getElementById('dropdownMenuIconButton');
     const dropdown = document.getElementById('dropdownDots');
+    const dropdownItems = dropdown.querySelectorAll('a');
 
+    // Toggle the dropdown menu when the button is clicked
     button.addEventListener('click', function () {
         dropdown.classList.toggle('hidden');
+    });
+
+    // Close the dropdown menu when an item is clicked
+    dropdownItems.forEach(item => {
+        item.addEventListener('click', function () {
+            dropdown.classList.add('hidden');
+        });
+    });
+
+    // Close the dropdown menu when clicking outside of it
+    document.addEventListener('click', function (event) {
+        const isClickInside = dropdown.contains(event.target) || button.contains(event.target);
+
+        if (!isClickInside) {
+            dropdown.classList.add('hidden');
+        }
     });
 });
